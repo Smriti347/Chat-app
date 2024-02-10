e2f58a4b453e937d5f3a1bb4e25390e393ce6f05
Smriti374 <202151159@iiitvadodara.ac.in>
2024-02-11 04:13:54 +0530

    first commit

---
+++ public/code.js
@@ -0,0 +1,82 @@
+(function(){
+
+    const app = document.querySelector(".app");
+    const socket = io();
+
+    let uname;
+
+    document.querySelector(".join-screen #join-user").addEventListener("click",function(){
+        let username = document.querySelector(".join-screen #username").value;
+        if(username.length == 0){
+            return;
+        }
+        socket.emit("newuser", username);
+        uname = username;
+        document.querySelector(".join-screen").classList.remove("active");
+        document.querySelector(".chat-screen").classList.add("active");
+    });
+
+    document.querySelector(".chat-screen #send-message").addEventListener("click", function(){
+        let message = document.querySelector(".chat-screen #message-input").value;
+        if(message.length == 0){
+            return;
+        }
+        renderMessage("my",{
+            username: uname,
+            text:message
+        });
+        socket.emit("chat",{
+            username: uname,
+            text: message
+        });
+        document.querySelector(".chat-screen #message-input").value = "";
+    });
+
+
+    document.querySelector(".chat-screen #exit-chat").addEventListener("click",function(){
+        socket.emit("exituser", uname);
+        window.location.href = window.location.href;
+    });
+
+    socket.on("update", function(update){
+        if(JSON.parse(JSON.stringify(update))?.username) renderMessage("other",JSON.parse(JSON.stringify(update)));
+        else renderMessage("update",update);
+    });
+
+    socket.on("chat", function(message){
+        console.log("on chat to other")
+        renderMessage("other",message);
+    });
+
+    function renderMessage(type, message){
+        let messageContainer = document.querySelector(".chat-screen .messages");
+        if(type == "my"){
+            console.log("my",JSON.stringify(message))
+            let e1 = document.createElement("div");
+            e1.setAttribute("class", "message my-message");
+            e1.innerHTML = `
+              <div>
+                <div class="name">You</div>
+                <div class="text">${message.text}</div>
+              </div>`;
+            messageContainer.appendChild(e1);
+        }else if(type == "other"){
+            console.log("other",JSON.stringify(message))
+            let e1 = document.createElement("div");
+            e1.setAttribute("class", "message other-message");
+            e1.innerHTML = `
+              <div>
+                <div class="name">${message.username}</div>
+                <div class="text">${message.text}</div>
+              </div>`;
+            messageContainer.appendChild(e1);
+        }else if(type == "update"){
+            console.log("update",JSON.stringify(message))
+            let e1 = document.createElement("div");
+            e1.setAttribute("class", "update");
+            e1.innerText = message;
+            messageContainer.appendChild(e1);
+        }
+        messageContainer.scrollTop = messageContainer.scrollHeight - messageContainer.clientHeight;
+    }
+})();
